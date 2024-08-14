5af1e05ca839e7cc0e571a6f28d4bc755a67dd21
Aravindhan-Rajendran <aravindhan12072000@gmail.com>
2024-08-14 18:56:58 +0530

    lkj

--- src/components/App.js
+++ src/components/App.js
@@ -33,7 +33,7 @@ const App = () => {
                 Clear Cart
             </button>
             <ul id="cart-items-list">
-                {state.items.length === 0 ? "" : (
+                {state.items.length === 0 ? "Cart is currently empty" : (
                     state.items.map((item) => (
                         <li key={item.id}>
                             <span id={`cart-item-name-${item.id}`}>{item.name}</span>
@@ -62,7 +62,7 @@ const App = () => {
                 )}
             </ul>
             <div id="cart-total-amount">
-                Total Amount: ${state.totalAmount}
+                ${state.totalAmount}
             </div>
         </div>
     );
