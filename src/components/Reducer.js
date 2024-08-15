23d37ad9066b4ef37bfa42258b10f55caa1e24a6
Aravindhan-Rajendran <aravindhan12072000@gmail.com>
2024-08-15 13:28:28 +0530

    d

--- src/components/Reducer.js
+++ src/components/Reducer.js
@@ -9,7 +9,7 @@ export const initialState = [
   {
     id: 2,
     title: "Google Pixel",
-    price: 499.98,
+    price: 499.99,
     img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1583371867/phone-1_gvesln.png",
     amount: 1
   },
