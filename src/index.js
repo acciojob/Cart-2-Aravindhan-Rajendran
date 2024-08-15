24bdbafa53205b0eb9002548cfed7f700150e54a
Aravindhan-Rajendran <aravindhan12072000@gmail.com>
2024-08-15 12:25:55 +0530

    dfd

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App'; // Adjust the path as per your structure
 
 ReactDOM.render(
-    <React.StrictMode>
-        <CartProvider>
-            <App />
-        </CartProvider>
-    </React.StrictMode>,
-    document.getElementById("root")
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
