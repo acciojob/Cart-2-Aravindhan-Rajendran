36c67397c766ee48af0da802f9ed067be72164a8
Aravindhan-Rajendran <aravindhan12072000@gmail.com>
2024-08-15 13:31:36 +0530

    Fd

--- src/components/Reducer.js
+++ src/components/Reducer.js
@@ -8,7 +8,7 @@ export const initialState = [
   },
   {
     id: 2,
-    title: "Google Pixel",
+    title: "google pixel ",
     price: 499.99,
     img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1583371867/phone-1_gvesln.png",
     amount: 1
@@ -16,7 +16,7 @@ export const initialState = [
   {
     id: 3,
     title: "Xiaomi Redmi Note 2",
-    price: 700.00,
+    price: 699.99,
     img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1583368224/phone-3_h2s6fo.png",
     amount: 1
   }
