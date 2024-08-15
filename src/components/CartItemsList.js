f5c75f3eb8fbbe02b4f30b6779d679b28e4a7672
Aravindhan-Rajendran <aravindhan12072000@gmail.com>
2024-08-15 12:09:56 +0530

    dsd

--- src/components/CartItemsList.js
+++ src/components/CartItemsList.js
@@ -1,39 +1,28 @@
-import React from 'react';
-import { useCart } from '../context/CartContext';
+export const initialState={
+  arraydata:[
+      {
+        id: 1,
+        title: "Samsung Galaxy S7",
+        price: 599.99,
+        img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1583368215/phone-2_ohtt5s.png",
+        amount: 1
+      },
+      {
+        id: 2,
+        title: "google pixel ",
+        price: 499.99,
+        img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1583371867/phone-1_gvesln.png",
+        amount: 1
+      },
+      {
+        id: 3,
+        title: "Xiaomi Redmi Note 2",
+        price: 699.99,
+        img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1583368224/phone-3_h2s6fo.png",
+        amount: 1
+      }
+    ]
+}
+export const Reducer=((state,action)=>{
 
-const CartItemsList = () => {
-  const { cart, dispatch } = useCart();
-
-  const handleIncrement = (id) => {
-    dispatch({ type: 'INCREMENT_ITEM', payload: { id } });
-  };
-
-  const handleDecrement = (id) => {
-    dispatch({ type: 'DECREMENT_ITEM', payload: { id } });
-  };
-
-  const handleRemove = (id) => {
-    dispatch({ type: 'REMOVE_ITEM', payload: { id } });
-  };
-
-  if (cart.length === 0) {
-    return <p>Cart is currently empty</p>;
-  }
-
-  return (
-    <ul id="cart-items-list">
-      {cart.map(item => (
-        <li key={item.id}>
-          <div>{item.name}</div>
-          <div id={`cart-item-price-${item.id}`}>{item.price}</div>
-          <div id={`cart-amount-${item.id}`}>{item.quantity}</div>
-          <button id={`increment-btn-${item.id}`} onClick={() => handleIncrement(item.id)}>+</button>
-          <button id={`decrement-btn-${item.id}`} onClick={() => handleDecrement(item.id)}>-</button>
-          <button onClick={() => handleRemove(item.id)}>Remove</button>
-        </li>
-      ))}
-    </ul>
-  );
-};
-
-export default CartItemsList;
+});
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import { CartProvider } from "./context/CartContext"; // Ensure the correct path here
 
 ReactDOM.render(
     <React.StrictMode>
